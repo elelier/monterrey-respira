5b51977a99eeab2fa59b22c16866a144073468e2
Elier Loya <92593786+elelier@users.noreply.github.com>
2026-05-05 05:04:48 -0600

    fix: add helmet provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+import { createRoot } from 'react-dom/client';
+import { HelmetProvider } from 'react-helmet-async';
+import './index.css';
+import App from './App.tsx';
 
-const rootElement = document.getElementById("root");
-if (!rootElement) {
-  throw new Error("Failed to find root element");
+const element = document.getElementById('root');
+
+if (!element) {
+  throw new Error('Root element was not found');
 }
 
-createRoot(rootElement).render(<App />);
+createRoot(element).render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>,
+);
